294206e2327b0e1f82cde180a6369b9c67f44c7f
Boyi Hao <bryce.hao@ticketmaster.com>
2019-09-18 12:19:05 -0400

    no message

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './style.scss';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+// <Link className="router-link" to="/">
+// Home
+// </Link>
   render() {
     return (
       <div className="header">
         <div className="header_introduce">
-          Hey, You finally find here.
+          Hey.
+        </div>
+        <div className="header_introduce">
+          You finally here.
         </div>
         <div className="header_introduce-line">
-           I`m Bryce, A Front End Engineer in ticketmaster.
+           I`m Bryce, A Software Engineer III from ticketmaster.
         </div>
         <div className="nav-bar">
-          <Link className="router-link" to="/">
-            Home
-          </Link>
         </div>
       </div>
     );
