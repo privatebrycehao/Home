5f85798c5f1e855dbf25899a53d077ba087500d3
Boyi Hao <bryce.hao@ticketmaster.com>
2019-09-17 22:41:15 -0400

    test

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -34,14 +34,12 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     return (
       <article>
         <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
+          <title>Bryce`s Home</title>
         </Helmet>
         <div className="home-page">
           <section className="centered">
-            <h2>Start your next react project in seconds</h2>
             <p>
-              A minimal <i>React-Redux</i> boilerplate with all the best practices
+             Will be my home
             </p>
           </section>
           <section>
