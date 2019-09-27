24cadc1361bd46757fe21d745ef3ce33f4ee0ac7
Boyi Hao <bryce.hao@ticketmaster.com>
2019-09-26 21:30:26 -0400

    Main page

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -2,21 +2,31 @@ import React from 'react';
 import './style.scss';
 import { SocialIcon } from 'react-social-icons';
 
-const Footer = () => (
-  <footer>
-    <section>This page is made by Bryce Hao.</section>
-    <div className="medias-wrapper">
-      <div className="medias-wrapper-single">
-        <SocialIcon url="https://www.linkedin.com/in/bryce-hao-1b5829bb/" />
-      </div>
-      <div className="medias-wrapper-single">
-        <SocialIcon url="https://www.facebook.com/myhaoboyi" />
-      </div>
-      <div className="medias-wrapper-single">
-        <SocialIcon url="https://github.com/privatebrycehao" />
-      </div>
-    </div>
-  </footer>
-);
+export default class Footer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.style = {
+      height: 25,
+      width: 25,
+    };
+  }
 
-export default Footer;
+  render() {
+    return (
+      <footer>
+        <section>This page is made by Bryce Hao.</section>
+        <div className="medias-wrapper">
+          <div className="medias-wrapper-single">
+            <SocialIcon style={this.style} url="https://www.linkedin.com/in/bryce-hao-1b5829bb/" />
+          </div>
+          <div className="medias-wrapper-single">
+            <SocialIcon style={this.style} url="https://www.facebook.com/myhaoboyi" />
+          </div>
+          <div className="medias-wrapper-single">
+            <SocialIcon style={this.style} url="https://github.com/privatebrycehao" />
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
